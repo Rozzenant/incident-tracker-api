8bae2ee66a5aa6d9571fc5bd92eee7875bb72ad3
Rozzenant <dmitry.pe4urckin@yandex.ru>
2025-11-07 04:58:56 +0300

    Created get API

--- incidents/views.py
+++ incidents/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Incident
+from .serializers import IncidentSerializer
 
-# Create your views here.
+
+class IncidentListView(generics.ListAPIView):
+    serializer_class = IncidentSerializer
+
+    def get_queryset(self):
+        queryset = Incident.objects.all().order_by('-created_at')
+        status = self.request.query_params.get('status')
+        if status:
+            queryset = queryset.filter(status=status)
+        return queryset
