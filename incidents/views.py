12f3c7d351f7503d525c14c2d61f793cdc143d91
Rozzenant <dmitry.pe4urckin@yandex.ru>
2025-11-07 05:33:55 +0300

    Added pagination with limit and offset for get API

--- incidents/views.py
+++ incidents/views.py
@@ -1,10 +1,16 @@
-from rest_framework import generics
+from rest_framework import generics, pagination
 from .models import Incident
 from .serializers import IncidentSerializer
 
 
+class IncidentPagination(pagination.LimitOffsetPagination):
+    default_limit = 20
+    max_limit = 100
+
+
 class IncidentListView(generics.ListAPIView):
     serializer_class = IncidentSerializer
+    pagination_class = IncidentPagination
 
     def get_queryset(self):
         queryset = Incident.objects.all().order_by('-created_at')
@@ -12,3 +18,4 @@ class IncidentListView(generics.ListAPIView):
         if status:
             queryset = queryset.filter(status=status)
         return queryset
+
