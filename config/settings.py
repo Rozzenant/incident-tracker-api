fd04c398b88515ca9250897e5bb37238a9a2cb46
Rozzenant <dmitry.pe4urckin@yandex.ru>
2025-11-07 05:00:37 +0300

    Hotfix settings file

--- config/settings.py
+++ config/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles' 'incidents',
+    'django.contrib.staticfiles',
+    'incidents',
 ]
 
 MIDDLEWARE = [
